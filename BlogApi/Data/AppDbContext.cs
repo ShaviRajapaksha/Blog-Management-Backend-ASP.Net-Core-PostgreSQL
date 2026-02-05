428c1616a9cb5b558c582383a22889173a9f3b46
IT20272654 <shavindurajapaksha@gmail.com>
2026-02-05 12:21:03 +0530

    Add AppDbContext class with DbSet properties for Users, Posts, and Comments

---
+++ BlogApi/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; } = null!;
+    public DbSet<Post> Posts { get; set; } = null!;
+    public DbSet<Comment> Comments { get; set; } = null!;
+}
